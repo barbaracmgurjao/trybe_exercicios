f85485bdd572c66c6c661d414f61740772b14a8c
Bárbara Cristina <barbaracmgurjao@gmail.com>
2022-04-18 15:13:11 -0300

    Questao 7.

--- trybe-exercicios/fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/forIn1.js
+++ trybe-exercicios/fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/forIn1.js
@@ -13,6 +13,13 @@ let leitor = {
 
   let livros = leitor.livrosFavoritos;
   console.log(livros);
-  for(key in livros){
+  for(let key in livros){
     console.log("O livro favorito de " + leitor.nome + ' ' + leitor.sobrenome + " se chama " + livros[key]['titulo']);
   }
+  livros.push({
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+  });
+
+    console.log(leitor.nome + " tem " + livros.length + " livros favoritos.")
