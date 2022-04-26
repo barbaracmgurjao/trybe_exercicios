a2d0da307b02f602bea781639be706b452c33f73
Bárbara Cristina <barbaracmgurjao@gmail.com>
2022-04-25 21:23:29 -0300

    Exercicio3 exemplos.

--- trybe-exercicios/fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
+++ trybe-exercicios/fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
@@ -4,3 +4,5 @@ let filme = document.getElementById('page-title');
 filme.innerText = 'Avengers';
 const segundoParagrafo = document.getElementById('second-paragraph');
 segundoParagrafo.style.background = 'green';
+const subtitle = document.getElementById('subtitle');
+subtitle.innerText = 'Este é um exemplo de subtítulo.';
