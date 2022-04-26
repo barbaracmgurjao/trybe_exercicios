607e11891a1ee27d162fbcdb17eb4bc9efca9ebc
Bárbara Cristina <barbaracmgurjao@gmail.com>
2022-04-25 21:21:59 -0300

    Exercicio2 exemplos.

--- trybe-exercicios/fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
+++ trybe-exercicios/fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
@@ -2,3 +2,5 @@ const paragraph = document.getElementById("paragraph");
 paragraph.style.color = "red";
 let filme = document.getElementById('page-title');
 filme.innerText = 'Avengers';
+const segundoParagrafo = document.getElementById('second-paragraph');
+segundoParagrafo.style.background = 'green';
