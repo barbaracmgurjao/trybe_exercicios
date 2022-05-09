b84ae30ed008c2f6528c5a4ebe9055be85fbf355
Bárbara Cristina <barbaracmgurjao@gmail.com>
2022-05-09 15:56:31 -0300

    .

---
+++ trybe-exercicios/fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/funcoes5.js
@@ -0,0 +1,20 @@
+function maisRepete(array){
+    let num = 0;
+    let numMaisRepetido = 0;
+    for(let i in array){
+        for()
+        if(num === 0){
+            num = 1;
+            numMaisRepetido = array[i];
+        }
+        else{
+            if(numMaisRepetido === array[i]){
+                num += 1;
+            }
+            else{
+
+            }
+        }
+    }
+}
+[2, 3, 2, 5, 8, 2, 3]
