861359a7886a8680278e53cb56c596a224fd228a
Bárbara Cristina <barbaracmgurjao@gmail.com>
2022-05-14 08:43:37 -0300

    bonus parte I

--- trybe-exercicios/fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/bonus1.js
+++ trybe-exercicios/fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/bonus1.js
@@ -18,20 +18,35 @@ const dragon = {
   damage: undefined,
 };
 
-const calculateDamage = (character) => {
-  let damage = 0;
-  if (character === 'dragon') {
-    damage = Math.floor((Math.random * dragon.strength) + 15);
-  } else if (character === 'warrior') {
-    damage = Math.floor(((Math.random * warrior.weaponDmg) + 1) * warrior.strength);
-  } else {
-    damage = Math.floor(((Math.random * 2) + 1) * mage.intelligence);
-  }
-  return damage;
+const dmg = (max, min) => { Math.floor(Math.random() * (max - min + 1) + min) };
+
+const dragonAttack = (dragon) => { 
+  const max = dragon.strength;
+  const min = 15;
+  dragon.damage = dmg(max, min);
 };
 
-const manaMage = () => (mage.mana >= 15 ? mage.mana -= 15 : mage.mana -= 0);
+const warriorAttack = (warrior) => { 
+  const max = warrior.strength * warrior.weaponDmg;
+  const min = warrior.strength;
+  warrior.damage = dmg(max, min);
+}
 
-const magePerShift = () => {return{ mana: manaMage(), damage: calculateDamage(mage)}};
+const manaMage = (min) => (mage.mana -= min);
+
+const mageAttack = (mage) => {
+  const min = mage.intelligence;
+  const max = 2 * min;
+  const consumedManaTurn = 15
+  if (mage.mana < consumedManaTurn) {
+    const notEnough = 'Não possui mana suficiente';
+    return  { mana: 0, damage: notEnough }; 
+  } else {
+    return { mana: manaMage(consumedManaTurn), damage: dmg(max, min) };
+  }
+};
 
 const battleMembers = { mage, warrior, dragon };
+
+
+
