22b949555744474adce299c7ed1a176e8ec22f16
Bárbara Cristina <barbaracmgurjao@gmail.com>
2022-05-23 15:57:20 -0300

    fixarDesc1.

---
+++ trybe-exercicios/fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixarDesc1.js
@@ -0,0 +1,16 @@
+
+const user = {
+  name: 'Maria',
+  age: 21,
+  nationality: 'Brazilian',
+};
+
+const jobInfos = {
+  profession: 'Software engineer',
+  squad: 'Rocket Landing Logic',
+  squadInitials: 'RLL',
+};
+
+const infoUser = { ...user, ...jobInfos};
+
+console.log(infoUser);
