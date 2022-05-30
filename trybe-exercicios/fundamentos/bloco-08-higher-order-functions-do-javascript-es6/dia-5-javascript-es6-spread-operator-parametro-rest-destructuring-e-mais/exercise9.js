60f5db8844a5995664afeb29aad81426cfab5fd1
Bárbara Cristina <barbaracmgurjao@gmail.com>
2022-05-30 09:12:58 -0300

    Exercise 9

---
+++ trybe-exercicios/fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise9.js
@@ -0,0 +1,9 @@
+const yearSeasons = {
+  spring: ['March', 'April', 'May'],
+  summer: ['June', 'July', 'August'],
+  autumn: ['September', 'October', 'November'],
+  winter: ['December', 'January', 'February'],
+};
+
+const { winter, spring, summer, autumn } = yearSeasons;
+const months = [...winter, ...spring, ...summer,...autumn];
